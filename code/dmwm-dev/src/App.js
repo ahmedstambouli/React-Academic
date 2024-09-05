31e6739e1c81c5beff8679fc2b2df154d750ca31
ahmed stambouli <ahmed.stambouli98@gmail.com>
2024-09-05 12:04:58 +0100

    terminer seance

---
+++ code/dmwm-dev/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import Button from './Componets/Button';
+
+function App() {
+  const titre = 'click this button';
+  return (
+    <div className="App">
+
+      <h1>welcom ahmed</h1>
+      <Button titre={titre}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ code/dmwm-dev/src/Componets/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+
+export default function Button({titre}) {
+  return (
+    <div>
+        <button >{titre}</button>
+    </div>
+  )
+}
+
